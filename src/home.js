77a247cc981135195e88d6b3acbe20b1f8fc47d7
DostonSuhrobov <doston.sanatovich@gmail.com>
2021-08-24 16:50:29 -0400

    1 binaries uploaded

--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import img from './bank.svg'
 import * as ReactBootstrap from 'react-bootstrap';
+
 
 
 
@@ -7,9 +9,9 @@ function Home() {
   return (
 
 <ReactBootstrap.Card className="bg-dark text-white">
-  <ReactBootstrap.Card.Img src="holder.js/100px270" alt="Card image" />
+  <ReactBootstrap.Card.Img src={img} alt="Card image" />
   <ReactBootstrap.Card.ImgOverlay>
-    <ReactBootstrap.Card.Title>Card title</ReactBootstrap.Card.Title>
+    <ReactBootstrap.Card.Title>Welcome</ReactBootstrap.Card.Title>
     <ReactBootstrap.Card.Text>
       This is a wider card with supporting text below as a natural lead-in to
       additional content. This content is a little bit longer.
