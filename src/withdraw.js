2b63b4b72bfdb9df0c1a3a596a67c58ac69411e2
DostonSuhrobov <doston.sanatovich@gmail.com>
2021-08-26 18:21:23 -0400

    withdraw page is ready

--- src/withdraw.js
+++ src/withdraw.js
@@ -1,11 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 
 import { UserContext } from './createContext.js';
 
 function Withdraw() {
   const [balance, setBalance] = useContext(UserContext);
-  
+  const [balance_1, setBalance1] = useState(0);
+
+  function calculate() {
+    setBalance(balance - balance_1);
+
+  }
 
   return (
     <div style={{ margin: 'auto', width: '400px' }}>
@@ -16,11 +21,13 @@ function Withdraw() {
           <ReactBootstrap.Form.Label>Withdraw Amount</ReactBootstrap.Form.Label>
           <ReactBootstrap.Form.Control
             type="number"
+            value={balance_1}
             placeholder="Enter your name"
+            onChange={e => setBalance1(parseInt(e.target.value))}
           />
         </ReactBootstrap.Form.Group>
 
-        <ReactBootstrap.Button variant="primary" type="submit">
+        <ReactBootstrap.Button variant="primary" onClick={calculate}>
           withdraw
         </ReactBootstrap.Button>
       </ReactBootstrap.Form>
