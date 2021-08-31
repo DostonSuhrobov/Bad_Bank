86e5e25f319f4ab2ea2e37a69e9b531fbe133c12
DostonSuhrobov <doston.sanatovich@gmail.com>
2021-08-31 17:46:52 -0400

    all pages ready

--- src/all_pages.js
+++ src/all_pages.js
@@ -5,41 +5,52 @@ import * as ReactBootstrap from 'react-bootstrap';
 import { UserContext } from './createContext.js';
 
 function AllPages() {
-  const [ info, setInfo ] = useContext(UserContext);
-  
+  const [info, setInfo] = useContext(UserContext);
 
   return (
-    <div style={{ margin: 'auto', width: '400px' }}>
-   <ReactBootstrap.Table striped bordered hover size="sm">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Email</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>{info[0]}</td>
-      <td>{info[1]}</td>
-      <td>{info[2]}</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>{info[3]}</td>
-      <td>{info[4]}</td>
-      <td>{info[5]}</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">{info[6]}</td>
-      <td>{info[7]}</td>
-    </tr>
-  </tbody>
-</ReactBootstrap.Table>
-      
+    <div style={{ margin: 'auto', width: '600px' }}>
+      <ReactBootstrap.Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Email</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>{info[0]}</td>
+            <td>{info[1]}</td>
+            <td>{info[2]}</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>{info[3]}</td>
+            <td>{info[4]}</td>
+            <td>{info[5]}</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>{info[6]}</td>
+            <td>{info[7]}</td>
+            <td>{info[8]}</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>{info[9]}</td>
+            <td>{info[10]}</td>
+            <td>{info[11]}</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>{info[12]}</td>
+            <td>{info[13]}</td>
+            <td>{info[14]}</td>
+          </tr>
+        </tbody>
+      </ReactBootstrap.Table>
     </div>
   );
 }
