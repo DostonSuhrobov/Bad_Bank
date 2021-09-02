3cb5f06feeafff916cf67b662ae312c8e734a964
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2021-09-02 19:40:50 -0400

    Update create_account.js

--- src/create_account.js
+++ src/create_account.js
@@ -1,95 +1,142 @@
 import React from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
-import * as Yup from 'yup';
-import { Formik, Field, Form } from 'formik';
+import { useFormik } from 'formik';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import { UserContext } from './createContext.js';
 
+
+const initialValues = {
+    name: '',
+    email: '',
+    password: ''
+  };
+  
+  const onSubmit = values => {
+    console.log('Form data : ', values);
+  };
+  
+  const validate = values => {
+    // values.name values.email values.password
+    // errors.name errors.email errors.chanel
+    // errors.name = 'This field is required'
+    let errors = {};
+  
+    if (!values.name) {
+      errors.name = 'Required';
+    }
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i.test(
+        values.email
+      )
+    ) {
+      errors.email = 'Not Valid Email';
+    }
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (!/^(?=.*\d).{8,}$/i.test(values.password)) {
+      errors.password = 'Minimum 8 characters required !';
+    }
+  
+    return errors;
+  };
+
+  
+
+
 function CreateAccount() {
+
   const [info, setInfo] = React.useContext(UserContext);
+
+
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    });
+
+    console.log('Form values : ', formik.values); // getting the values from fornik
+    console.log('Form Errors : ', formik.errors); // this one is for seeing the errrors
+
+    function thisOne(e) {
+        e.preventDefault();
+        setInfo([...info,formik.values.name,formik.values.email, formik.values.password]);
+        formik.values.name = '';
+        formik.values.email = '';
+        formik.values.password = '';
+    }
+
+
 
   return (
     <div style={{ margin: 'auto', width: '400px' }}>
-      <Formik
-        initialValues={{ name: '', email: '', subject: '' }}
-        onSubmit={values => {
-          setInfo([...info, values.name, values.email, values.subject]);
-        }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .max(15, 'Must be 15 characters or less')
-            .required('Name is required'),
-          subject: Yup.string()
-            .required('Subject is required')
-            .min(8),
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('Email is required')
-        })}
-      >
-        {formik => (
-          <Form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <Field
-                name="name"
-                className={
-                  formik.touched.name && formik.errors.name
-                    ? 'form-control is-invalid'
-                    : 'form-control'
-                }
-                type="text"
-              />
-              {formik.touched.name && formik.errors.name ? (
-                <div className="invalid-feedback">{formik.errors.name}</div>
-              ) : null}
-            </div>
 
-            <div className="form-group">
-              <label htmlFor="email">Email Address</label>
-              <Field
+        <ReactBootstrap.Form>
+        <ReactBootstrap.Form.Group className="mb-3" controlId="formBasicEmail">
+                
+                <ReactBootstrap.Form.Label>Email name</ReactBootstrap.Form.Label>
+                <ReactBootstrap.Form.Control
+                 type="text"
+                 name="name"
+                 placeholder="Enter name" 
+                 onChange={formik.handleChange} 
+                 value={formik.values.name}/>
+
+                 {formik.errors.name ? (
+                    <div style={{ color: 'red' }}> {formik.errors.name}</div>
+                    ) : null}
+
+            </ReactBootstrap.Form.Group>
+
+            <ReactBootstrap.Form.Group className="mb-3" controlId="formBasicEmail">
+                
+                <ReactBootstrap.Form.Label>Email address</ReactBootstrap.Form.Label>
+                <ReactBootstrap.Form.Control 
+                type="email" 
                 name="email"
-                className={
-                  formik.touched.email && formik.errors.email
-                    ? 'form-control is-invalid'
-                    : 'form-control'
-                }
-                type="email"
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <div className="invalid-feedback">{formik.errors.email}</div>
-              ) : null}
-            </div>
+                placeholder="Enter email" 
+                onChange={formik.handleChange} 
+                value={formik.values.email} />
 
-            <div className="form-group">
-              <label htmlFor="subject">Password</label>
-              <Field
-                name="subject"
-                className={
-                  formik.touched.subject && formik.errors.subject
-                    ? 'form-control is-invalid'
-                    : 'form-control'
-                }
-                type="password"
-              />
-              {formik.touched.subject && formik.errors.subject ? (
-                <div className="invalid-feedback">{formik.errors.subject}</div>
-              ) : null}
-            </div>
+                {formik.errors.email ? (
+                    <div style={{ color: 'red' }}> {formik.errors.email}</div>
+                    ) : null}     
 
-            <br />
+            </ReactBootstrap.Form.Group>
 
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary">
-                Create Account
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+            <ReactBootstrap.Form.Group className="mb-3" controlId="formBasicPassword">
+
+                <ReactBootstrap.Form.Label>Password</ReactBootstrap.Form.Label>
+                <ReactBootstrap.Form.Control 
+                type="password" 
+                name="password"
+                placeholder="Password" 
+                onChange={formik.handleChange} 
+                value={formik.values.password} />
+
+                {formik.errors.password ? (
+                    <div style={{ color: 'red' }}> {formik.errors.password}</div>
+                    ) : null}   
+
+
+            </ReactBootstrap.Form.Group>
+
+
+            <ReactBootstrap.Button variant="primary" type="submit" disabled={
+            (!formik.values.name, !formik.values.email, !formik.values.password)
+          }
+            onClick={thisOne}
+          >
+                Submit
+            </ReactBootstrap.Button>
+        </ReactBootstrap.Form>
+
     </div>
-  );
+);
 }
 
 export default CreateAccount;
