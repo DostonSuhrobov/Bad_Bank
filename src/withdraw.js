e2d2fddb45f0bea805a87cc874c8801d53e68f90
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2021-09-02 20:43:20 -0400

    Update withdraw.js

--- src/withdraw.js
+++ src/withdraw.js
@@ -3,6 +3,10 @@ import * as ReactBootstrap from 'react-bootstrap';
 
 import { UserContext } from './createContext.js';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function Withdraw() {
   const [balance, setBalance] = useContext(UserContext);
   const [balance_1, setBalance1] = useState(0);
@@ -10,6 +14,35 @@ function Withdraw() {
   function calculate() {
     setBalance(balance - balance_1);
     setBalance1(0);
+
+    console.log(balance)
+
+    if (balance <= 0){
+
+      toast.warn('You have low funds!!!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+
+    }
+
+    else if( balance > 0 ){
+
+      toast.success('Balance Updated!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      });
+    }
 
   }
 
@@ -22,15 +55,29 @@ function Withdraw() {
           <ReactBootstrap.Form.Label>Withdraw Amount</ReactBootstrap.Form.Label>
           <ReactBootstrap.Form.Control
             type="number"
+            min='0'
             value={balance_1}
             placeholder="Enter your name"
             onChange={e => setBalance1(parseInt(e.target.value))}
           />
         </ReactBootstrap.Form.Group>
 
-        <ReactBootstrap.Button variant="primary" onClick={calculate}>
+        <ReactBootstrap.Button variant="primary" onClick={calculate} disabled={!balance_1}>
           withdraw
         </ReactBootstrap.Button>
+
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+
       </ReactBootstrap.Form>
     </div>
   );
