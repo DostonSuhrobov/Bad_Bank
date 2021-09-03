6c690f25843369425d212249b84bbcc1b949a5a0
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2021-09-02 20:42:57 -0400

    Update deposit.js

--- src/deposit.js
+++ src/deposit.js
@@ -3,12 +3,31 @@ import * as ReactBootstrap from 'react-bootstrap';
 
 import { UserContext } from './createContext.js';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function Deposit() {
   const [balance, setBalance] = useContext(UserContext);
   const [balance_1, setBalance1] = useState(0);
 
   function calculate() {
+
     setBalance(balance + balance_1);
+    console.log(balance);
+ 
+      toast.success('Balance Updated!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      });
+
+    
+
+
     setBalance1(0);
   }
 
@@ -21,15 +40,29 @@ function Deposit() {
           <ReactBootstrap.Form.Label>Deposit Amount</ReactBootstrap.Form.Label>
           <ReactBootstrap.Form.Control
             type="number"
+            min='0'
             value={balance_1}
             onChange={e => setBalance1(parseInt(e.target.value))}
             placeholder="Enter deposit balanse"
           />
         </ReactBootstrap.Form.Group>
 
-        <ReactBootstrap.Button variant="primary" onClick={calculate}>
+        <ReactBootstrap.Button variant="primary" onClick={calculate} disabled={!balance_1}>
           Deposit
         </ReactBootstrap.Button>
+
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+
       </ReactBootstrap.Form>
     </div>
   );
