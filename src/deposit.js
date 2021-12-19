336233cfc6b7ed89b7a6a3e856301401b193b7ba
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2021-12-19 09:04:43 -0500

    Update deposit.js

--- src/deposit.js
+++ src/deposit.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { createElement, useContext, useState } from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 
 import { UserContext } from './createContext.js';
@@ -6,15 +6,69 @@ import { UserContext } from './createContext.js';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+import { getDatabase, ref, onValue, set } from "firebase/database";
+
+import { initializeApp } from 'firebase/app'
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqg7LLfd49TuQE_Kn8AifWXhzJNkTB7KA",
+  authDomain: "react-firechat-f3539.firebaseapp.com",
+  projectId: "react-firechat-f3539",
+  storageBucket: "react-firechat-f3539.appspot.com",
+  messagingSenderId: "571094429249",
+  appId: "1:571094429249:web:ae5ed730a0a2d8743260c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
 function Deposit() {
-  const [balance, setBalance] = useContext(UserContext);
+  const [balance, setBalance] = useState(0);
   const [balance_1, setBalance1] = useState(0);
+
+
+  // reading the data section 
+  function balance_disp(data){
+
+    console.log(data);
+    
+    document.getElementById('here').textContent = data;
+    document.getElementById('here').value = data;
+
+  }
+
+  
+
+  const db = getDatabase();
+  const balanceRef = ref(db, 'balance/balance');
+  onValue(balanceRef, (snapshot) => {        
+    
+    const data = snapshot.val();
+    balance_disp(data);
+  })
+
+
+// writing the data to the firebase
+
 
   function calculate() {
 
-    setBalance(balance + balance_1);
-    console.log(balance);
- 
+
+      let balance_ = document.getElementById('here').value;
+      let total_balance = balance_1 + balance_;
+      
+        const db = getDatabase();
+        set(ref(db, 'balance/'), {
+          balance:total_balance
+        });
+
+
       toast.success('Balance Updated!', {
         position: 'top-center',
         autoClose: 5000,
@@ -26,14 +80,16 @@ function Deposit() {
       });
 
     
-
-
-    setBalance1(0);
   }
 
   return (
     <div style={{ margin: 'auto', width: '400px' }}>
-      <h1>Balanace: {balance} </h1>
+      {
+
+
+      }
+      
+      <h1>Balanace: <p id='here'> </p> </h1>
       <br />
       <ReactBootstrap.Form>
         <ReactBootstrap.Form.Group className="mb-3" controlId="formBasicEmail">
